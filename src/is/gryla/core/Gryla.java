75a1ccdf8cb26efa2cc4322263b2dea22710e52d
kristjan-halldorsson <kristjanh@gmail.com>
2010-11-21 18:33:49 +0000

    Initial commit! :)

---
+++ src/is/gryla/core/Gryla.java
@@ -0,0 +1,43 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Kristjan
+ * Date: 10.11.2010
+ * Time: 15:04:38
+ * To change this template use File | Settings | File Templates.
+ */
+package is.gryla.core;
+
+import is.iclt.icenlp.core.tokenizer.Sentences;
+import is.iclt.icenlp.core.icetagger.IceTagger;
+import is.iclt.icenlp.facade.IceTaggerFacade;
+import is.iclt.icenlp.facade.IceParserFacade;
+
+class Gryla {
+    public static void main(String[] args) {
+
+        //TODO: Ætli það sé betra að nota StringBuffer???
+        String inputText = args[0];
+        String outTaggedAndParsedtext = "";
+
+        //Jájá
+
+
+        try {
+            IceTaggerFacade tagger = new IceTaggerFacade(IceTagger.HmmModelType.startend);
+            IceParserFacade parser = new IceParserFacade();
+
+            //Here we tag the text and get it back in a Sentence form.
+            Sentences sentences = tagger.tag(inputText);
+            //Next we send the Sentences as string to the parser and get back a string.
+            outTaggedAndParsedtext = parser.parse(sentences.toString(), true, true);
+
+            System.out.println("Tagged text: " + sentences.toString());
+
+        } catch (Exception e) {
+            System.err.println("Error: " + e);
+        }
+
+        System.out.println("Tagged and parsed: \n" + outTaggedAndParsedtext);
+    }
+
+}
