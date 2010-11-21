81abc559bd9be49d8d6a2a88698502e7654bf841
kristjan-halldorsson <kristjanh@gmail.com>
2010-11-21 19:28:41 +0000

    Breytti intakinu og úttakinu. Grýlan tekur nú við streng frá commandpromt og skilar bara því sem kemur frá IceNLP til baka á skjá (eða sem úttak sem plugin hlustar á).

--- src/is/gryla/core/Gryla.java
+++ src/is/gryla/core/Gryla.java
@@ -1,10 +1,10 @@
 /**
  * Created by IntelliJ IDEA.
- * User: Kristjan
+ * User: Kristján og Skúli
  * Date: 10.11.2010
  * Time: 15:04:38
- * To change this template use File | Settings | File Templates.
  */
+
 package is.gryla.core;
 
 import is.iclt.icenlp.core.tokenizer.Sentences;
@@ -16,12 +16,13 @@ class Gryla {
     public static void main(String[] args) {
 
         //TODO: Ætli það sé betra að nota StringBuffer???
-        //String inputText = args[0];
-        String inputText = "Test bla";
-        String outTaggedAndParsedtext = "";
-
-        //Jáján fjlasdhjkasdhjkashdjkahsjd
-
+        String inputText = "";
+        //Input should be a text inside parentheses ""
+        if (args.length == 1){
+            inputText = args[0];
+        }
+        //inputText = "Test lína til að parsa og tagga ef það sé ekkert inntak.";
+        String outTaggedAndParsedText = "";
 
         try {
             IceTaggerFacade tagger = new IceTaggerFacade(IceTagger.HmmModelType.startend);
@@ -29,16 +30,15 @@ class Gryla {
 
             //Here we tag the text and get it back in a Sentence form.
             Sentences sentences = tagger.tag(inputText);
-            //Next we send the Sentences as string to the parser and get back a string.
-            outTaggedAndParsedtext = parser.parse(sentences.toString(), true, true);
-
-            System.out.println("Tagged text: " + sentences.toString());
-
+            //Next we send the Sentences as strings to the parser and get back a string.
+            outTaggedAndParsedText = parser.parse(sentences.toString(), true, true);
         } catch (Exception e) {
-            System.err.println("Error: " + e);
+            System.out.println("Tag and parsing error.");
+            //System.err.println("Error: " + e);
         }
 
-        System.out.println("Tagged and parsed: \n" + outTaggedAndParsedtext);
+        //Write the output to the screen.
+        System.out.println(outTaggedAndParsedText);
     }
 
 }
